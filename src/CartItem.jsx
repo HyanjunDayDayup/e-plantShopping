9e1d54d3d1537df57e89a96eabdd9c8063db231c
HyanjunDayDayup <hyanjun546@Gmail.com>
2024-10-02 23:41:40 -0400

    change my code

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
+import { useNavigate } from 'react-router-dom';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((total,item) => total + item.cost *item.quantity,0);
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault();
+    navigate('./ProductList')  
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({id:item.id,quantity:item.quantity +1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity >1){
+        dispatch(updateQuantity({id:item.id,quantity:item.quantity -1}));   
+    }
+    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.id));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.cost * item,quantity;
   };
 
   return (
